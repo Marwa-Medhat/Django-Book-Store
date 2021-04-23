b5a056691084eba69549a0e7a3c562565749f6fc
Marwa Medhat <eng.marwamedhat2020@gmail.com>
2021-04-24 01:48:56 +0200

    signals

--- books/models.py
+++ books/models.py
@@ -2,44 +2,46 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 # Create your models here.
+
+
 class Tag (models.Model):
-     name=models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
-     def __str__(self):
-          return self.name   
-
+    def __str__(self):
+        return self.name
 
 
 class Category (models.Model):
-     class Meta:
-          verbose_name_plural="categories"
+    class Meta:
+        verbose_name_plural = "categories"
 
-     name=models.CharField(max_length=50)
-     created_at=models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-     def __str__(self):
-          return self.name   
+    def __str__(self):
+        return self.name
 
 
 class Isbin (models.Model):
-     book_title=models.CharField(max_length=255,null=True,blank=True)
-     author_title=models.CharField(max_length=50)
-     booknumber = models.UUIDField(default=uuid.uuid4, editable=False)
+    book_title = models.CharField(max_length=255, null=True, blank=True)
+    author_title = models.CharField(max_length=50)
+    booknumber = models.UUIDField(default=uuid.uuid4, editable=False)
 
+    def __str__(self):
+        return f"{self. author_title} author_title | {self.  booknumber} booknumber "
 
-     def __str__(self):
-          return f"{self. author_title} author_title | {self.  booknumber} booknumber "  
 
 class Book (models.Model):
-     # title=models.CharField(max_length=255)
-     content=models.TextField(max_length=2048)
-     Categories=models.ManyToManyField(Category)
-     author=models.ForeignKey(User,on_delete=models.CASCADE,null=True,related_name="books")
-     isbin=models.OneToOneField(Isbin,on_delete=models.CASCADE,null=True,blank=True)
-     # isbin=models.OneToOneField(Isbin,on_delete=models.CASCADE,default=default)
-     tag=models.ForeignKey(Tag,on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    content = models.TextField(max_length=2048)
+    Categories = models.ManyToManyField(Category)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="books")
+    isbin = models.OneToOneField(
+        Isbin, on_delete=models.CASCADE, null=True, blank=True)
+    # isbin=models.OneToOneField(Isbin,on_delete=models.CASCADE,default=default)
+    tag = models.ForeignKey(
+        Tag, on_delete=models.CASCADE, null=True, blank=True)
 
-
-
-     def __str__(self):
-          return self.title    
+    def __str__(self):
+        return self.title
---
+++ books/signals.py
@@ -0,0 +1,20 @@
+#  signal for create isbn objects then assign it to create book
+
+from django.db.models.signals import post_save, pre_save, post_delete, pre_delete
+from django.dispatch import receiver
+from .models import Book, Isbin, User
+
+# after save ..sender is book
+# created when book save
+
+
+@receiver(post_save, sender=Book)
+def after_book_creation(sender, instance, created, *args, **kwargs):
+    # when create book makes isbin related to it
+    if created:
+        isbn_instance = Isbin.objects.create(
+            author_title=instance.author.username, book_title=instance.title)
+        instance.isbin = isbn_instance
+        instance.save()
+    else:
+        print("updating")
